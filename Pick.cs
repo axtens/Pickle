a539e69eeb9f10bc751e41e4da7a28d6433603ab
Bruce Axtens <bruce.axtens@gmail.com>
2024-05-16 10:28:23 +0800

    Add project files.

---
+++ Pick.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Forms;
+
+namespace Pickle
+{
+    public static class Pick
+    {
+        public static string GetFile(string title, string folder, string filter = "\"PR files (*.pr)|*.pr|P files (*.p)|*.p|All files (*.*)|*.*\"", int index = 1)
+        {
+            OpenFileDialog getter = new()
+            {
+                CheckFileExists = true,
+                CheckPathExists = true,
+                Title = title,
+                FilterIndex = index,
+                Filter = filter,
+                InitialDirectory = folder,
+                ShowHelp = false,
+                ShowPinnedPlaces = false
+            };
+
+            getter.ShowDialog();
+            return getter.FileName;
+        }
+    }
+}
